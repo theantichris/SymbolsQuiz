4f2128b152126d2ef9f3d7aa5bdfca20d08979c0
Christopher Lamm <christopher.lamm@companycam.com>
2023-11-27 11:07:11 -0500

    added list of symbol and variable to track the one being viewed

--- SymbolsQuiz/ViewController.swift
+++ SymbolsQuiz/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let symbolList = ["A", "B", "C", "D", "E", "F", "G", "H", "IJ", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "UV", "W", "X", "Y", "Z", "End of sentence"]
+    var currentSymbolIndex = 0
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var answerLabel: UILabel!
